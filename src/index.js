e0c3e8df44cafce342e2f9d339c1cfede21ccec5
EduhuSua <Eduardosuarez.ucv@gmail.com>
2021-07-21 14:53:13 -0400

    Terminando funcionalidades y añadiendo algo de css

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function Player(props){
     return(
         <div className="cont-player" onClick={props.function}>
             <div className="cont-image">
-                <img src={props.data.img}/>
+                <img src={props.data.img} alt="Player"/>
             </div>
             <div className="cont-description">
                 <p>{props.data.nombre}</p>
@@ -34,55 +34,59 @@ function ContPlayers(props){
 function App(props){
 
     const Personas = [
-        {id: 0, nombre:"Eduardo", img:"0.jpg"},
-        {id: 1, nombre:"Daniel", img:"1.jpg"},
-        {id: 2, nombre:"Pedro", img:"2.jpg"},
-        {id: 3, nombre:"Alicia", img:"3.jpg"},
-        {id: 4, nombre:"Katherine", img:"4.jpg"},
+        {id: 0, nombre:"Eduardo", img:"./img/1.jpg"},
+        {id: 1, nombre:"Daniel", img:"./img/2.jpg"},
+        {id: 2, nombre:"Pedro", img:"./img/3.jpg"},
+        {id: 3, nombre:"Alicia", img:"./img/.jpg"},
+        {id: 4, nombre:"Katherine", img:"./img/5.jpg"},
     ]
-
     const Team1 = []
-    Team1.push({id: 5, nombre:"CAPI", img:"5.jpg"})
+    Team1.push({id: 5, nombre:"CAPI", img:"./img/6.jpg"})
     const Team2 = []
-    Team2.push({id: 5, nombre:"CAPI", img:"5.jpg"})
+    Team2.push({id: 5, nombre:"CAPI", img:"./img/7.jpg"})
 
     const [board,setBoard] = useState(Personas);
     const [team1,setTeam1] = useState(Team1);
     const [team2,setTeam2] = useState(Team2);
     const [turnTeam1,setTurn] = useState(true);
 
-    // function removePlayer1(id){
-    //     // const newTeam =  team1.slice()
-    //     // newTeam.splice(i)
-    //     // setTeam1(newTeam)
-    // }
+    function removePlayer1(id){
+        const player = Personas.filter(item => item.id === id)
+        const newTeam1 = team1.filter(item => item.id !== id)
+        setTeam1(newTeam1[0])
 
-    // function removePlayer2(id){
-    //     // const newTeam =  team2.slice()
-    //     // newTeam.splice(i)
-    //     // setTeam2(newTeam)
-    // }
+        const newBoard = board.push(player[0])
+        setBoard(newBoard)
+    }
+
+    function removePlayer2(id){
+        const player = Personas.filter(item => item.id === id)
+        const newTeam2 = team2.filter(item => item.id !== id)
+        setTeam1(newTeam2[0])
+        
+        const newBoard = board.push(player[0])
+        setBoard(newBoard)
+    }
 
     function addPlayer(id){
-        console.log(id)
         if(turnTeam1){
             const player = Personas.filter(item => item.id === id)
             const newBoard = board.filter(item => item.id !== id)
             const newTeam = team1.slice()
             newTeam.push(player[0])
+            console.log("Team1")
+            console.log(newTeam)
             setTeam1(newTeam)
             setBoard(newBoard)
-            console.log("Team1")
-            console.log(team1)
         }else{
             const player = Personas.filter(item => item.id === id)
             const newBoard = board.filter(item => item.id !== id)
             const newTeam = team2.slice()
             newTeam.push(player[0])
+            console.log("Team2")
+            console.log(newTeam)
             setTeam2(newTeam)
             setBoard(newBoard)
-            console.log("Team2")
-            console.log(team2)
         }
         setTurn(!turnTeam1)
     }
@@ -91,8 +95,8 @@ function App(props){
     }
     return(
         <div className="main-cont">
-            <ContPlayers key={0} data={Team1} function={ret}/>
-            <ContPlayers key={1} data={Team2} function={ret}/>
+            <ContPlayers key={0} data={team1} function={ret}/>
+            <ContPlayers key={1} data={team2} function={ret}/>
             <ContPlayers 
                 key={2} 
                 data={board}  
