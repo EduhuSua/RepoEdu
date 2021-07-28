8fe335b60d1ea78cbbe366b94546ea288a953532
EduhuSua <Eduardosuarez.ucv@gmail.com>
2021-07-28 00:01:26 -0400

    Terminando ando el proyectando

--- src/index.js
+++ src/index.js
@@ -1,22 +1,61 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+
+import img1 from "./img/1.jpg"
+import img2 from "./img/2.jpg"
+import img3 from "./img/3.jpg"
+import img5 from "./img/5.jpg"
+import img4 from "./img/4.jpg"
+import img6 from "./img/6.jpg"
+import img7 from "./img/7.jpg"
+
 import './index.css';
 
+
+
 function Player(props){
-    return(
-        <div className="cont-player" onClick={props.function}>
-            <div className="cont-image">
-                <img src={props.data.img} alt="Player"/>
+    const capitan = props.data.capitan
+    if(capitan){
+        return(
+            <div className="cont-player">
+                <div className="cont-image">
+                    <img src={props.data.img} alt="Player"/>
+                </div>
+                <div className="cont-description">
+                    <p>{props.data.nombre}</p>
+                </div>
             </div>
-            <div className="cont-description">
-                <p>{props.data.nombre}</p>
-                <p>{props.data.id}</p>
+        )
+    }else{
+        return(
+            <div className="cont-player" onClick={props.function}>
+                <div className="cont-image">
+                    <img src={props.data.img} alt="Player"/>
+                </div>
+                <div className="cont-description">
+                    <p>{props.data.nombre}</p>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 function ContPlayers(props){
+    const isTeam  = props.team
+    if(isTeam){
+    return(
+        <div className="cont-teams team"> 
+            {
+                props.data.map(player =>(
+                    <Player 
+                        key={player.id} 
+                        data={player} 
+                        function={() => props.function(player.id)}/>
+                ))
+            }
+        </div>
+    )
+    }else{
     return(
         <div className="cont-teams"> 
             {
@@ -28,54 +67,55 @@ function ContPlayers(props){
                 ))
             }
         </div>
-    )
+        )
+    }
 }
 
 function App(props){
 
     const Personas = [
-        {id: 0, nombre:"Eduardo", img:"./img/1.jpg"},
-        {id: 1, nombre:"Daniel", img:"./img/2.jpg"},
-        {id: 2, nombre:"Pedro", img:"./img/3.jpg"},
-        {id: 3, nombre:"Alicia", img:"./img/.jpg"},
-        {id: 4, nombre:"Katherine", img:"./img/5.jpg"},
+        {id: 0, nombre:"Eduardo", img: img1, capitan: false},
+        {id: 1, nombre:"Daniel", img: img2, capitan: false},
+        {id: 2, nombre:"Pedro", img: img3, capitan: false},
+        {id: 3, nombre:"Alicia", img: img4, capitan: false},
+        {id: 4, nombre:"Katherine", img: img5, capitan: false},
     ]
     const Team1 = []
-    Team1.push({id: 5, nombre:"CAPI", img:"./img/6.jpg"})
+    Team1.push({id: 5, nombre:"CAPI", img: img6, capitan: true})
     const Team2 = []
-    Team2.push({id: 5, nombre:"CAPI", img:"./img/7.jpg"})
+    Team2.push({id: 6, nombre:"CAPI", img: img7, capitan: true})
 
     const [board,setBoard] = useState(Personas);
     const [team1,setTeam1] = useState(Team1);
     const [team2,setTeam2] = useState(Team2);
     const [turnTeam1,setTurn] = useState(true);
+    const [turno,setTurno] = useState(true);
 
     function removePlayer1(id){
         const player = Personas.filter(item => item.id === id)
+        const newBoard = board.slice() 
         const newTeam1 = team1.filter(item => item.id !== id)
-        setTeam1(newTeam1[0])
-
-        const newBoard = board.push(player[0])
+        newBoard.push(player[0])
+        setTeam1(newTeam1)
         setBoard(newBoard)
     }
 
     function removePlayer2(id){
         const player = Personas.filter(item => item.id === id)
+        const newBoard = board.slice() 
         const newTeam2 = team2.filter(item => item.id !== id)
-        setTeam1(newTeam2[0])
-        
-        const newBoard = board.push(player[0])
+        newBoard.push(player[0])
+        setTeam2(newTeam2)
         setBoard(newBoard)
     }
 
     function addPlayer(id){
+        setTurno(!turno)
         if(turnTeam1){
             const player = Personas.filter(item => item.id === id)
             const newBoard = board.filter(item => item.id !== id)
             const newTeam = team1.slice()
             newTeam.push(player[0])
-            console.log("Team1")
-            console.log(newTeam)
             setTeam1(newTeam)
             setBoard(newBoard)
         }else{
@@ -83,8 +123,6 @@ function App(props){
             const newBoard = board.filter(item => item.id !== id)
             const newTeam = team2.slice()
             newTeam.push(player[0])
-            console.log("Team2")
-            console.log(newTeam)
             setTeam2(newTeam)
             setBoard(newBoard)
         }
@@ -93,14 +131,24 @@ function App(props){
     function ret(){
         return
     }
+    let valorTurno = ""
+    if(turno){
+        valorTurno = "Equipo de la Izq"
+    }else{
+        valorTurno = "Equipo de la Der"
+    }
     return(
-        <div className="main-cont">
-            <ContPlayers key={0} data={team1} function={ret}/>
-            <ContPlayers key={1} data={team2} function={ret}/>
-            <ContPlayers 
-                key={2} 
-                data={board}  
-                function={addPlayer}/>
+        <div className="intentoBody">
+            <div className="header">
+                <h1>Seleccionador poderoso de equipos</h1>
+                <hr/>
+            </div>
+            <div className="main-cont">
+                <ContPlayers key={0} data={team1} function={removePlayer1} team={true}/>
+                <ContPlayers key={1} data={team2} function={removePlayer2} team={true}/>
+                <ContPlayers key={2} data={board} function={addPlayer} team={false}/>
+            </div>
+            <h2>Turno de {valorTurno}</h2>
         </div>
     );
 }
